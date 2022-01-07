7ccd675bcb5179f31bc30bf73e39ad4bd4bca5a3
Oleksii Bondarenko <oleksii.bondarenko@almaviacx.com>
2022-01-07 19:08:57 +0200

    Add Send message callback

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,7 @@ import s from './Dialogs.module.css';
 import {NavLink, Link} from "react-router-dom";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import React from "react";
 
 const Dialogs = (props) => {
 
@@ -9,12 +10,19 @@ const Dialogs = (props) => {
     let dialogsElements = props.state.dialogs.map (d => <DialogItem name={d.name} id={d.id}/>)
     let messagesElements = props.state.messages.map(m => <Message message={m.message}/>)
 
+    let NewMessage = React.createRef();
+    let SendMessage = () => {
+        let text = NewMessage.current.value
+        alert(text)
+    }
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 {dialogsElements}
             </div>
             <div className={s.messages}>
+                <textarea ref={NewMessage}></textarea>
+                <button onClick={SendMessage}>Send Message</button>
                 {messagesElements}
             </div>
         </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,14 +1,19 @@
 import s from './MyPosts.module.css'
 import Post from './Post/Post'
+import React from "react";
 
 const MyPosts = (props) => {
     let postsElements = props.posts.map(p => <Post message={p.message} count={p.likesCount}/>)
-    const addPost = () => {alert('Hello Oleksii')}
+    let newPostElement = React.createRef();
+    const addPost = () => {
+        let text = newPostElement.current.value
+        alert(text)
+    }
     return (
         <div>
             My Posts
             <div>
-                <textarea></textarea>
+                <textarea ref={newPostElement}></textarea>
                 <button onClick={addPost}>Add Post</button>
             </div>
             <div className={s.posts}>
