534e4a1e5d4b1ed4a70798347dc9b9c98009b755
Oleksii Bondarenko <oleksii.bondarenko@almaviacx.com>
2022-01-07 18:51:25 +0200

    onClick eventListener move into separete function

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,12 +3,13 @@ import Post from './Post/Post'
 
 const MyPosts = (props) => {
     let postsElements = props.posts.map(p => <Post message={p.message} count={p.likesCount}/>)
+    const addPost = () => {alert('Hello Oleksii')}
     return (
         <div>
             My Posts
             <div>
                 <textarea></textarea>
-                <button onClick={() => alert('Hi Oleksii')}>Add Post</button>
+                <button onClick={addPost}>Add Post</button>
             </div>
             <div className={s.posts}>
                 {postsElements}
