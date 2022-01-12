1697fc06082b546e5a5121ee1becad2714628543
Oleksii Bondarenko <oleksii.bondarenko@almaviacx.com>
2022-01-12 18:39:13 +0200

    npm i redux --save, implement redux for state managment

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -3,7 +3,26 @@ import {render} from "react-dom";
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'ADD-NEW-MESSAGE';
 
-const dialogsReducer = (state, action) => {
+let initialState =  {
+    dialogs: [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrew'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'}
+    ],
+    messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your it-kamasutra?'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yo'}
+    ],
+    newMessageBody: 'Hey that is my new message to you',
+}
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
             state.newMessageBody = action.body
