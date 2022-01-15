23b555d851261e175eb3e014c9fc2a140acd947e
Oleksii Bondarenko <oleksii.bondarenko@almaviacx.com>
2022-01-15 16:45:06 +0200

    Update users-reducer.js

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,24 +1,50 @@
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const FOLLOW = 'FOLLOW';
+const UNFOLLOW = 'UNFOLLOW';
+const SET_USERS = 'SET_USERS';
 
 let initialState = {
     users: [
-        {id: 1, name: 'Oleksii909', status: 'I am professional FullStack Developer (Drupal+React)', location: {city:'Kiev', country: 'Ukraine'}},
-        {id: 2, name: 'Andrey', status: 'I am drupal developer', location: {city:'Moscow', country: 'Russia'}},
-        {id: 3, name: 'Dmitriy', status: 'I am react developer', location: {city:'Minsk', country: 'Belarus'}},
+        {id: 1, followed: true, name: 'Oleksii909', status: 'I am professional FullStack Developer (Drupal+React)', location: {city:'Kiev', country: 'Ukraine'}},
+        {id: 2, followed: false, name: 'Andrey', status: 'I am drupal developer', location: {city:'Moscow', country: 'Russia'}},
+        {id: 3, followed: true, name: 'Dmitriy', status: 'I am react developer', location: {city:'Minsk', country: 'Belarus'}},
     ],
 };
 
 const usersReducer = (state = initialState, action) => {
     switch(action.type) {
+        case FOLLOW :
+            return {
+                ...state,
+                users: state.users.map( u => {
+                    if (u.id === action.userId) {
+                        return {...u, followed: true}
+                    }
+                    return u
+                })
+            }
+        case UNFOLLOW :
+            return {
+                ...state,
+                users: state.users.map( u => {
+                    if (u.id === action.userId) {
+                        return {...u, followed: false}
+                    }
+                    return u
+                })
+            }
+        case SET_USERS :
+            return {
+                ...state,
+                users: [...state.users, ...action.users]
+            }
         default:
             return state;
     }
 }
 
 
-export const addPostActionCreator = () => ({type: ADD_POST})
-export const updateNewPostTextActionCreator = (text) =>
-    ({type: UPDATE_NEW_POST_TEXT, newText: text })
+export const followAC = (userId) => ({type: FOLLOW, userId})
+export const unfollowAC = (userId) => ({type: UNFOLLOW, userId})
+export const setUsersAC = (users) => ({type: SET_USERS, users})
 
 export default usersReducer;
